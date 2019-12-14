3eb7bf37b91886920ed22ffa930be57cd1d73251
Zachary Velcoff <33340277+zackfactual@users.noreply.github.com>
2019-12-14 15:39:56 -0600

    initial commit

---
+++ js/thelivingisez.js
@@ -0,0 +1,34 @@
+$(document).ready(function() { 
+    // auto-update copyright
+    var year = (new Date).getFullYear();
+    $("#copy").append("&copy; " + year + " — Zachary Velcoff");
+});
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 27, 2020 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + " days " + hours + " hours "
+  + minutes + " minutes and " + seconds + " seconds to go!";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "We did it!";
+  }
+}, 1000);
