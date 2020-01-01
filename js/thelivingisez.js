68168afab3d00812dc05cc7049471035be05b375
Zachary Velcoff <33340277+zackfactual@users.noreply.github.com>
2020-01-01 14:47:36 -0600

    Complete front page first draft

--- js/thelivingisez.js
+++ js/thelivingisez.js
@@ -23,8 +23,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + " days " + hours + " hours "
-  + minutes + " minutes and " + seconds + " seconds to go!";
+  document.getElementById("demo").innerHTML = days + " days<br />" + hours + " hours<br />"
+  + minutes + " minutes<br />" + seconds + " seconds!";
 
   // If the count down is finished, write some text
   if (distance < 0) {
