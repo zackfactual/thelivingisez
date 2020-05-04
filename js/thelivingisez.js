a4b0fbf728b769202912d2383397fb0708eeed7e
Elena Ondich <eondich@gmail.com>
2020-05-03 22:30:03 -0500

    Some lodging etc code, COVID banner

--- js/thelivingisez.js
+++ js/thelivingisez.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 27, 2020 16:00:00").getTime();
+var countDownDate = new Date("Jul 23, 2021 16:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
